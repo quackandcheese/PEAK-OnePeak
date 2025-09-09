50db074235e9f784e190969f1baab5ef6675aa60
quackandcheese <quackandcheeseart@gmail.com>
2025-09-09 12:14:23 -0500

    start on stretchy arm

--- OnePeak/src/OnePeak/Patches/CharacterMovement_Patch.cs
+++ OnePeak/src/OnePeak/Patches/CharacterMovement_Patch.cs
@@ -11,34 +11,6 @@ namespace OnePeak.Patches
     [HarmonyPatch(typeof(CharacterMovement))]
     internal class CharacterMovement_Patch
     {
-/*        [HarmonyPrefix]
-        [HarmonyPatch(nameof(CharacterMovement.CheckFallDamage))]
-        static bool CheckFallDamage_Prefix(ref CharacterMovement __instance)
-        {
-            if (__instance.character.refs.afflictions.GetCurrentStatus(Plugin.GumGumStatus) > 0.0)
-            {
-
-                if (__instance.FallTime() > __instance.fallDamageTime)
-                {
-                    float fallDamageAmount = Mathf.Max(__instance.FallFactor(3f, 1.5f), 0.05f);
-                    fallDamageAmount = Mathf.Min(fallDamageAmount, __instance.MaxVelDmg());
-                    //fallDamageAmount *= Ascents.fallDamageMultiplier;
-                    if (fallDamageAmount >= 0.025f)
-                    {
-                        Debug.Log("Fall damage: " + (fallDamageAmount * 100f).ToString("F0") + "%");
-                        if (fallDamageAmount > 0.3f && __instance.character.IsLocal)
-                        {
-                            __instance.character.Fall(fallDamageAmount * 5f, 0f);
-                            __instance.character.AddForce((__instance.character.Center - __instance.character.data.groundPos).normalized * 15000 * fallDamageAmount);
-                        }
-                    }
-                }
-
-                return false;
-            }
-
-            return true;
-        }*/
         [HarmonyPrefix]
         [HarmonyPatch(nameof(CharacterMovement.Land))]
         static bool Land_Prefix(ref CharacterMovement __instance, ref CharacterMovement.PlayerGroundSample bestSample)
---
+++ OnePeak/src/OnePeak/Patches/Item_Patch.cs
@@ -0,0 +1,32 @@
+﻿using DG.Tweening;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using Zorro.Core;
+
+namespace OnePeak.Patches
+{
+    [HarmonyPatch(typeof(Item))]
+    public class Item_Patch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(Item.Interact))]
+        static void Interact_Postfix(ref Item __instance, ref Character interactor)
+        {
+            if (interactor.refs.afflictions.GetCurrentStatus(Plugin.GumGumStatus) > 0.0)
+            {
+                Vector3 itemPosition = __instance.transform.position;
+                Rigidbody elbow = interactor.GetBodypartRig(BodypartType.Elbow_R);
+
+                Tweener tween = elbow.DOMove(itemPosition, 1);
+            }
+            //tween.OnComplete(() => elbow.DOMove(itemPosition, 1));
+            //elbow.DORestart();
+            //DOTween.Play();
+
+
+        }
+    }
+}
