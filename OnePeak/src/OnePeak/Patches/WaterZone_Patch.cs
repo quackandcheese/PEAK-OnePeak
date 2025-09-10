3f1eba2e85ae1ff9e2b3a020a4ade34b12931f35
quackandcheese <quackandcheeseart@gmail.com>
2025-09-09 21:27:57 -0500

    add water drowsiness for gum gum fruit

---
+++ OnePeak/src/OnePeak/Patches/WaterZone_Patch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using Peak.Afflictions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace OnePeak.Patches;
+
+[HarmonyPatch(typeof(WaterZone))]
+public class WaterZone_Patch
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(WaterZone.Update))]
+    static void UpdatePatch_Postfix(WaterZone __instance)
+    {
+        if (Character.localCharacter == null)
+        {
+            return;
+        }
+        if (__instance.characterInsideBounds && Character.observedCharacter == Character.localCharacter && Character.localCharacter.refs.afflictions.GetCurrentStatus(Plugin.GumGumStatus) > 0.0)
+        {
+            float drowsinessPerSecond = 0.05f;
+            Character.localCharacter.refs.afflictions.AddStatus(CharacterAfflictions.STATUSTYPE.Drowsy, drowsinessPerSecond * Time.deltaTime, false);
+        }
+    }
+}
